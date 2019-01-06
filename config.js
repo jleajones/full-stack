7e9eb0cea70bf5505963dab4a9e972adb918c7e3
Jason Lea-Jones <jleajones@gmail.com>
2019-01-06 12:30:46 -0500

    updates to use .config

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 import convict from 'convict';
 
 const config = convict({
-    env: {
-        doc: 'The Application Environment',
-        format: ['PROD', 'DEV', 'TEST'],
-        default: 'dev',
-        env: 'NODE_ENV'
-    },
-    port: {
-        doc: 'The Application Port',
-        format: 'port',
-        default: 5000,
-        env: 'PORT'
-    }
+  env: {
+    doc: 'The Application Environment',
+    format: ['PROD', 'DEV', 'TEST'],
+    default: 'dev',
+    env: 'NODE_ENV'
+  },
+  port: {
+    doc: 'The Application Port',
+    format: 'port',
+    default: 5000,
+    env: 'PORT'
+  }
 });
 
 const env = config.get('env');
-config.loadFile(`./config/${env}.json`);
+config.loadFile(`./.config/${env}.json`);
 config.validate({ allowed: 'strict' });
 
 export default config;
