9a84ccc011f73c1e5478d236617bac15c83d10a2
Jason Lea-Jones <jason.leajones@vroom.com>
2019-01-14 13:12:25 -0500

    better comments

--- client/server/index.js
+++ client/server/index.js
@@ -10,21 +10,23 @@ import logger from '../../utils/logger';
 import { errLog, outLog } from '../../middleware/logger';
 import serverRenderer from './middleware/serverRenderer';
 
+// Grab configs
 const ENV = config.get('env');
 const PORT = config.get('port');
 
+// Read Cert files
 const certOptions = {
   key: fs.readFileSync(path.resolve('../server.key')),
   cert: fs.readFileSync(path.resolve('../server.crt'))
 };
 
-// initialize the application and create the routes
+// Create App, initialize logging
 const app = express();
 app.use(errLog);
 app.use(outLog);
 
+// Create Router
 const router = express.Router();
-
 // root (/) should always serve our server rendered page
 router.use('^/$', serverRenderer);
 
@@ -33,11 +35,13 @@ router.use(
   express.static(path.resolve(__dirname, '..', 'build'), { maxAge: '30d' })
 );
 
+// other routes fall back to renderer
 router.use(serverRenderer);
+
 // tell the app to use the above rules
 app.use(router);
 
-// const httpServer = http.createServer(app);
+// create https server
 const httpsServer = https.createServer(certOptions, app);
 
 // start the app
